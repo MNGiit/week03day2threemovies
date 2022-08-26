741cf226bfa018d67d0ee95241826973ad7ddc3e
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-25 22:44:10 -0400

    add app.js, and prepare method

---
+++ app.js
@@ -0,0 +1,17 @@
+// Get the button first
+const scrollUpBtn = document.getElementById("");
+
+// When user moves down from the top of the doc, show the button
+// Method will show button to scroll up by checking the document scroll distance
+const toggleScrollUpBtn = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { scrollUpBtn.style.display = "block";} 
+    else { scrollUpBtn.style.display = "none"; }
+}
+
+// Method to send user back to the top of the document
+const goToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {toggleScrollUpBtn};
